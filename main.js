67181891b9fd0dd4c1460260545cacd234f0fe8f
jw4ng <jadenwang19@gmail.com>
2023-11-17 16:34:37 -0500

    Added changes to footer

--- main.js
+++ main.js
@@ -312,6 +312,7 @@ class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <footer class="bg-light">
+                <br>
                 <img src="../images/VAEnergyLogo.png" class="va-energy-logo"> <img src="../images/VTLogo.png" class="vt-logo">
                 <br><br> Feedback or Questions? Contact <a href="https://energytransition.coopercenter.org/profile/elizabeth-marshall">Elizabeth Marshall</a> at the Virginia Solar Intiative
             </footer>
